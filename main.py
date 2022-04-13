dbc53ea089d56c48d0518edf461b7927f1b8e8e5
Badw0lf613 <badwolf613@shu.edu.cn>
2022-04-13 19:42:34 +0800

    st.write split

--- main.py
+++ main.py
@@ -20,12 +20,23 @@ def get_subdirs(b='.'):
             result.append(bd)
     return result
 
+def get_subdirs_without_labels(b='.'):
+    '''
+        Returns all sub-directories in a specific Path
+    '''
+    result = []
+    for d in os.listdir(b):
+        bd = os.path.join(b, d)
+        if os.path.isdir(bd):
+            # result.append(bd)
+            break
+    return result
 
 def get_detection_folder():
     '''
         Returns the latest folder in a runs\detect
     '''
-    return max(get_subdirs(os.path.join('runs', 'detect')), key=os.path.getmtime)
+    return max(get_subdirs_without_labels(os.path.join('runs', 'detect')), key=os.path.getmtime)
 
 
 if __name__ == '__main__':
