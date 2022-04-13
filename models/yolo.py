971a2a424a286f9d0714e1905502c6f3e6e7d3db
Badw0lf613 <badwolf613@shu.edu.cn>
2022-04-13 17:57:51 +0800

    Update yolo feature_visualization

--- models/yolo.py
+++ models/yolo.py
@@ -28,7 +28,7 @@ from models.common import *
 from models.experimental import *
 from utils.autoanchor import check_anchor_order
 from utils.general import make_divisible
-from utils.plots import feature_visualization
+# from utils.plots import feature_visualization
 from utils.torch_utils import (fuse_conv_and_bn, initialize_weights, model_info, profile, scale_img, select_device,
                                time_sync)
 
@@ -162,8 +162,8 @@ class Model(nn.Module):
                 self._profile_one_layer(m, x, dt)
             x = m(x)  # run
             y.append(x if m.i in self.save else None)  # save output
-            if visualize:
-                feature_visualization(x, m.type, m.i, save_dir=visualize)
+            # if visualize:
+            #     feature_visualization(x, m.type, m.i, save_dir=visualize)
         return x
 
     def _descale_pred(self, p, flips, scale, img_size):
