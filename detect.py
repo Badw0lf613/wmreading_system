669bd5af4d9421cfb16527af75fdca3ea647261f
Badw0lf613 <badwolf613@shu.edu.cn>
2022-04-13 19:08:31 +0800

    opt.save_txt True

--- detect.py
+++ detect.py
@@ -16,7 +16,8 @@ from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
 def detect(opt, save_img=False):
-    source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
+    # source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
+    source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, True, opt.img_size
     save_img = not opt.nosave and not source.endswith(
         '.txt')  # save inference images
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
