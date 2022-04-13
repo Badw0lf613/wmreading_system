c5de8976c38c8655c2c9f101cab9e4cf3d63d95c
姚施越 <Badwolf613@shu.edu.cn>
2022-04-13 12:13:27 +0800

    Update water_meter_app.py

--- water_meter_app.py
+++ water_meter_app.py
@@ -78,7 +78,7 @@ def load_model(src, path, device, reload=False):
 @st.cache
 def detect(weights, source):
 #   os.system("python yolov5-master/detect.py --weights %s --source %s" % (weights, source))
-  im0 = detect_yolov5.main_yolov5(weights, source)
+  im0 = detect_yolov5.run(weights, source)
   fig = plt.figure(figsize=(7, 7))
   plt.imshow(im0)
   plt.axis('off')
