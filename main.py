15b695b1fb91e637dc1e4b7b29fc741257aa9cba
Badw0lf613 <badwolf613@shu.edu.cn>
2022-04-13 16:18:41 +0800

    modify pt

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str,
-                        # default='weights/best.pt', help='model.pt path(s)')
-                        default='weights/yolov5s.pt', help='model.pt path(s)')
+                        default='weights/best.pt', help='model.pt path(s)')
+                        # default='weights/yolov5s.pt', help='model.pt path(s)')
     parser.add_argument('--source', type=str,
                         default='data/images', help='source')
     parser.add_argument('--img-size', type=int, default=640,
