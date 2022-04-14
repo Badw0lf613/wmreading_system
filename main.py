8615c4fd6a5c2315a7c58e94498574cc32a91f03
Badw0lf613 <badwolf613@shu.edu.cn>
2022-04-14 15:10:31 +0800

    save_conf

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ if __name__ == '__main__':
                       columns=['Labels', 'Position', 'Confidenc'],
                       index=np.linspace(1, len(line_list), len(line_list), dtype=int))
                     i = 0
-                    for (l, p, c) in zip(line_list, line_list1, line_list2):
+                    for (l, p, c) in zip(line_list, line_list2, line_list3):
                         df.iloc[i,0] = l
                         df.iloc[i,1] = p
                         df.iloc[i,2] = c
