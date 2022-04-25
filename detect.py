a1a7557717e40afb2adc255d16727164fad6a704
Badw0lf613 <badwolf613@shu.edu.cn>
2022-04-25 11:00:34 +0800

    mp4

--- detect.py
+++ detect.py
@@ -156,7 +156,8 @@ def detect(opt, save_img=False):
                             h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
                         else:  # stream
                             fps, w, h = 30, im0.shape[1], im0.shape[0]
-                            save_path += '.mp4'
+                            # save_path += '.mp4'
+                            save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
                         print('!!!vid save_path', save_path)
                         vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                         # vid_writer = cv2.VideoWriter(
